9742c580235985283ee2d4bdee6281b2df5bd045
Nikomaru <nikomaru@nikomaru.dev>
2022-12-25 01:15:43 +0900

    Fix horse index page load lazy.

--- pages/horse/index.tsx
+++ pages/horse/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import styles from "../../styles/Home.module.css";
 import Head from "next/head";
 import Navbar from "../../components/Navbar";
@@ -109,7 +109,7 @@ Number.prototype.toRound = function (base: Number) {
     return Math.floor(this.valueOf() * Math.pow(10, base.valueOf())) / Math.pow(10, base.valueOf());
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
     // ファイル名が [id].tsx なので id パラメーターを取り出すことができる
     const res = await fetch(process.env.RACEASSIST_API_WEBHOOK_URL + "/v1/horse/listAll");
     let horseData = (await res.json()) as HorseData[];
@@ -118,6 +118,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         props: {
             props: { data: horseData },
         },
+        revalidate: 1800,
     };
 };
 
